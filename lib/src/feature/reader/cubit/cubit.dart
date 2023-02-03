f54786f41e64e546188cb0dd65122b6482654598
Onnimanni Hannonen <onnari11@gmail.com>
2023-02-03 17:09:31 +0200

    fully implements QrPlusRenderer and starts the implementation of QrPlusReader

--- lib/src/feature/reader/cubit/cubit.dart
+++ lib/src/feature/reader/cubit/cubit.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:qr_plus/src/model/model.dart';
 import 'package:qr_plus/src/model/src/qr_plus_data.dart';
 
 part 'qr_plus_reader_cubit.dart';
